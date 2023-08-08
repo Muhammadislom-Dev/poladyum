710c236f182f19b1fd27011d8b1daa834eccb566
MuhammadislomDev <97617526+Muhammadislom-Dev@users.noreply.github.com>
2023-08-08 18:01:03 +0500

    Update Footer.jsx

--- src/Layouts/Footer/Footer.jsx
+++ src/Layouts/Footer/Footer.jsx
@@ -10,8 +10,8 @@ function Footer() {
           <div className="footer_flex">
             <div className="footer_left">
               <h3>
-                O‘zbekiston Respublikasi, Farg‘ona viloyati, Beshariq shahri,
-                Sirdaryo ko‘chasi, 46-uy
+                O‘zbekiston Respublikasi, Samarqand viloyati, Samarqand shahri,
+                Beruniy ko‘chasi
               </h3>
               <h3>Dushanbadan shanbagacha soat 8:00 dan 17:00 gacha</h3>
             </div>
